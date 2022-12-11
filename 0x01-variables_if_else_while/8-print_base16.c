cdd16cbe1864323db4d42b3ffdbdf2a06fb3f7b9
Ejemai14 <ejemaimoses@gmail.com>
2022-12-10 22:48:44 -0800

    base sisteen

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char b;
+
+	int c;
+
+	b = 'a';
+	c = 0;
+
+	while (c < 10)
+	{
+		putchar(c + '0');
+		c++;
+	}
+	while (b <= 'f')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
