46366d4af3bc3a38eba9430bda8b968af882ab32
Ejemai14 <ejemaimoses@gmail.com>
2022-12-14 05:55:10 -0800

    alph

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char b = 'a';
+
+	while (b <= z)
+	{
+		_putchar(b);
+		b++;
+	}
+	_putchar('\n');
+	return (0);
+}
