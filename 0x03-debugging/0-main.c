611323425703d019820f43699afff483fa97d081
Ejemai14 <ejemaimoses@gmail.com>
2022-12-15 13:19:06 -0800

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test for a postitive_or_negative function
+ * Return: zero
+ */
+
+int main(void)
+
+{
+	int o;
+
+	o = 0;
+	positive_or_negative(o);
+
+	return (0);
+}
